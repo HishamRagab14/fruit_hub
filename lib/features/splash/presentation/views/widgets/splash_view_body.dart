410bfadfe9b8a47ad288591a656618f412af0b9f
HishamRagab14 <hishamragab14@yahoo.com>
2025-01-19 06:48:20 +0200

    Add Assets class and update SplashView to use SplashViewBody
    Created an `Assets` class to manage asset paths and updated the `SplashView` to include a `SplashViewBody` widget that displays the app logo, plant icon, and splash bubble using SVG images. Also, added the `flutter_svg` dependency to handle SVG assets.

---
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fruits_hub_app/core/utils/assets.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SvgPicture.asset(Assets.imagesPlant),
+          ],
+        ),
+        SvgPicture.asset(Assets.imagesLogo),
+        SvgPicture.asset(Assets.imagesSplashBubble,fit: BoxFit.fill,)
+      ],
+    );
+  }
+}

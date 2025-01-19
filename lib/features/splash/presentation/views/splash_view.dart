e649035a3aca7484d2b8c2a85508dafb04c64c62
HishamRagab14 <hishamragab14@yahoo.com>
2025-01-19 06:02:26 +0200

    Add onGenerateRoute for route management
    Implemented onGenerateRoute function to handle routing in the app. Added a static routeName to SplashView and configured MaterialApp to use onGenerateRoute and initialRoute.

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -2,11 +2,9 @@ import 'package:flutter/material.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
-
+  static const routeName = 'splash';
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
-    );
+    return const Scaffold();
   }
 }

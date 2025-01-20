e1710305b2b3ae8c25a2aa32e534cf24ac5eb69c
HishamRagab14 <hishamragab14@yahoo.com>
2025-01-20 19:55:28 +0200

    Add custom app bar and login view body
    Implemented a custom app bar for the login view and created a new `LoginViewBody` widget. Also, added the `SharedPreferencesPlugin` registration in `GeneratedPluginRegistrant.swift`.

---
+++ lib/core/widgets/custom_app_bar.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub_app/core/utils/app_text_styles.dart';
+
+AppBar createAppBar(BuildContext context,{required String title}) {
+    return AppBar(
+      leading: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: const Icon(Icons.arrow_back_ios_new)),
+      title:  Text(
+        title,
+        style: TextStyles.bold19,
+        textAlign: TextAlign.center,
+      ),
+      centerTitle: true,
+    );
+  }
--- lib/features/authentication/presentation/views/login_view.dart
+++ lib/features/authentication/presentation/views/login_view.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub_app/core/widgets/custom_app_bar.dart';
+import 'package:fruits_hub_app/features/authentication/presentation/views/widgets/login_view_body.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
- static const routeName = 'login';
+  static const routeName = 'login';
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: createAppBar(title: 'تسجيل الدخول',context),
+      body: const LoginViewBody(),
+    );
   }
+
+  
 }

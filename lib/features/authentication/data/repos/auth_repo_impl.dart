6c3ce923b763627284d56112718543f1bf0f78da
HishamRagab14 <hishamragab14@yahoo.com>
2025-01-21 17:45:12 +0200

    Add user model and implement createUserWithEmailAndPassword in AuthRepoImpl
    - Created `user_model.dart` to define the user model.
    - Updated `auth_repo_impl.dart` to include `FirebaseAuthService` dependency and implement the `createUserWithEmailAndPassword` method.
    - Added necessary dependencies (`flutter_bloc`, `bloc`, `nested`, `provider`) in `pubspec.yaml` and `pubspec.lock`.

--- lib/features/authentication/data/repos/auth_repo_impl.dart
+++ lib/features/authentication/data/repos/auth_repo_impl.dart
@@ -1,14 +1,36 @@
 import 'package:dartz/dartz.dart';
+import 'package:fruits_hub_app/core/errors/exceptions.dart';
 import 'package:fruits_hub_app/core/errors/failures.dart';
+import 'package:fruits_hub_app/core/services/firebase_auth_service.dart';
+import 'package:fruits_hub_app/features/authentication/data/models/user_model.dart';
 import 'package:fruits_hub_app/features/authentication/domain/entites/user_entity.dart';
 import 'package:fruits_hub_app/features/authentication/domain/repos/auth_repo.dart';
 
-class AuthRepoImpl extends AuthRepo{
+class AuthRepoImpl extends AuthRepo {
+  final FirebaseAuthService firebaseAuthService;
+
+  AuthRepoImpl({required this.firebaseAuthService});
   @override
-  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword({ String email, String password}) {
-    
-    throw UnimplementedError();
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
+      String email, String password,String name) async {
+    try {
+      var user = await firebaseAuthService.createUserWithEmailAndPassword(
+          email: email, password: password);
+      return Right(
+        UserModel.fromFirebaseUser(user),
+      );
+    } on CustomException catch (e) {
+      return Left(
+        ServerFailure(
+          e.message,
+        ),
+      );
+    } catch (e) {
+      return Left(
+        ServerFailure(
+          'An error occurred while creating the user.',
+        ),
+      );
+    }
   }
-
-
 }

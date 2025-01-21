203d4e7c3e4a84b7b59999199e745a966f187c0e
HishamRagab14 <hishamragab14@yahoo.com>
2025-01-21 20:56:08 +0200

    Add buildErrorBar function and CustomBlocObserver
    - Introduced `buildErrorBar` function in `build_error_bar.dart` to handle error snackbars.
    - Created `CustomBlocObserver` class in `custom_bloc_observer.dart` to log Bloc changes and transitions in debug mode.
    - Updated `signup_view_body_bloc_consumer.dart` to use `buildErrorBar` for displaying error messages.
    - Modified `main.dart` to set `CustomBlocObserver` as the Bloc observer.

--- lib/features/authentication/presentation/views/signup_view_body_bloc_consumer.dart
+++ lib/features/authentication/presentation/views/signup_view_body_bloc_consumer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_hub_app/core/helper_functions/build_error_bar.dart';
 import 'package:fruits_hub_app/features/authentication/presentation/manager/signup_cubit/signup_cubit_cubit.dart';
 import 'package:fruits_hub_app/features/authentication/presentation/views/widgets/signup_view_body.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
@@ -13,19 +14,20 @@ class SignupViewBodyBlocConsumer extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocConsumer<SignupCubit, SignupState>(
       listener: (context, state) {
-        if(state is SignupLoading){}
-        if(state is SignupSuccess){
+        if (state is SignupSuccess) {
           Navigator.pop(context);
         }
-        if(state is SignupFailure){
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(state.errMsg)));
+        if (state is SignupFailure) {
+          buildErrorBar(context, state.errMsg);
         }
       },
       builder: (context, state) {
         return ModalProgressHUD(
-          inAsyncCall: state is SignupLoading ? true : false,
-          child: const SignupViewBody());
+            inAsyncCall: state is SignupLoading ? true : false,
+            child: const SignupViewBody());
       },
     );
   }
+
+  
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruits_hub_app/core/helper_functions/on_generate_route.dart';
+import 'package:fruits_hub_app/core/services/custom_bloc_observer.dart';
 import 'package:fruits_hub_app/core/services/get_it_service.dart';
 import 'package:fruits_hub_app/core/services/shared_preference_singleton.dart';
 import 'package:fruits_hub_app/core/utils/app_colors.dart';
@@ -11,6 +13,7 @@ import 'package:fruits_hub_app/firebase_options.dart';
 import 'generated/l10n.dart';
 
 void main() async{
+  Bloc.observer = CustomBlocObserver();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,

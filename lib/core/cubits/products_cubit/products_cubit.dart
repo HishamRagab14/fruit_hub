43ebb397a8ee48622bea156ab8b11df664a60642
HishamRagab14 <hishamragab14@yahoo.com>
2025-02-03 16:15:34 +0200

    Add ProductsCubit and ProductsState classes
    Introduce the `ProductsCubit` class to manage product-related state using the BLoC pattern. Also, add the `ProductsState` class with an initial state `ProductsInitial`.

---
+++ lib/core/cubits/products_cubit/products_cubit.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_hub_app/core/entities/product_entity.dart';
+import 'package:fruits_hub_app/core/repos/products_repo/products_repo.dart';
+
+part 'products_state.dart';
+
+class ProductsCubit extends Cubit<ProductsState> {
+  ProductsCubit(this.productsRepo) : super(ProductsInitial());
+  final ProductsRepo productsRepo;
+  Future<void> getBestSellingProducts() async {
+    emit(ProductsLoading());
+    final result = await productsRepo.getProducts();
+    result.fold((failure) {
+      emit(ProductsFailure(failure.message));
+    }, (products) {
+      emit(ProductsSuccess(products));
+    });
+  }
+}
---
+++ lib/core/cubits/products_cubit/products_state.dart
@@ -0,0 +1,18 @@
+part of 'products_cubit.dart';
+
+sealed class ProductsState {}
+
+final class ProductsInitial extends ProductsState {}
+
+final class ProductsLoading extends ProductsState {}
+
+final class ProductsFailure extends ProductsState {
+  final String errMsg;
+  ProductsFailure(this.errMsg);
+}
+
+final class ProductsSuccess extends ProductsState {
+  final List<ProductEntity> products;
+
+  ProductsSuccess(this.products);
+}
